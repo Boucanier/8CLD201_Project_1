78422d311203383a1ecaecff769f1756c8aeb626
Jules Chiron <chironjules.78@gmail.com>
2024-11-08 17:38:50 -0500

    Correction : js packages link

--- installation.sh
+++ installation.sh
@@ -11,7 +11,7 @@ mkdir /home/azureadmin/website
 
 # Téléchargement des fichiers du site web
 wget https://raw.githubusercontent.com/Boucanier/8CLD201_Project_1/main/website/app.js -O /home/azureadmin/website/app.js
-wget https://raw.githubusercontent.com/Boucanier/8CLD201_Project_1/website/package.json -O /home/azureadmin/website/package.json
+wget https://raw.githubusercontent.com/Boucanier/8CLD201_Project_1/main/website/package.json -O /home/azureadmin/website/package.json
 
 # On se place dans le dossier du site web
 cd /home/azureadmin/website
