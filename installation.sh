1ad27083fcb70e6aed471adafa47914bea4eabd6
Jules Chiron <chironjules.78@gmail.com>
2024-11-08 22:26:34 -0500

    Upgraded website

--- installation.sh
+++ installation.sh
@@ -6,11 +6,18 @@ sudo apt-get update
 # Installation de Node.js et npm
 sudo apt-get install -y nodejs npm
 
-# Création du dossier du site web
+# Création des dossiers du site web
 mkdir /home/azureadmin/website
+mkdir /home/azureadmin/website/views
+mkdir /home/azureadmin/website/public
+mkdir /home/azureadmin/website/public/images
+mkdir /home/azureadmin/website/public/styles
 
 # Téléchargement des fichiers du site web
 wget https://raw.githubusercontent.com/Boucanier/8CLD201_Project_1/main/website/app.js -O /home/azureadmin/website/app.js
+wget https://raw.githubusercontent.com/Boucanier/8CLD201_Project_1/main/website/public/images/UQAC_logo.png -O /home/azureadmin/website/public/images/UQAC_logo.png
+wget https://raw.githubusercontent.com/Boucanier/8CLD201_Project_1/main/website/public/styles/style.css -O /home/azureadmin/website/public/styles/style.css
+wget https://raw.githubusercontent.com/Boucanier/8CLD201_Project_1/main/website/views/index.ejs -O /home/azureadmin/website/views/index.ejs
 wget https://raw.githubusercontent.com/Boucanier/8CLD201_Project_1/main/website/package.json -O /home/azureadmin/website/package.json
 
 # On se place dans le dossier du site web
--- website/app.js
+++ website/app.js
@@ -1,15 +1,23 @@
 // app.js
 const os = require('os');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 80;
 
+// Configurer le moteur de template EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Servir les fichiers statiques depuis le dossier "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
     const hostname = os.hostname();
-    res.send(`<h1>Nom de la machine : ${hostname}</h1>`);
+    res.render('index', { hostname });
 });
 
 app.listen(port, () => {
-    console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
